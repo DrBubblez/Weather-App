f53be4630e8af62ea7c405df3364677cb0e727c9
DrBubblez <harrisonminer59@gmail.comgit config --global uder.name DrBubblezgit config --global user.email harrisonminer59@gmail.com>
2023-09-29 02:24:53 -0400

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,47 @@
+// On page load, blur the search input to hide the keyboard on mobile
 window.onload = function() {
     document.activeElement.blur();
 };
-$(document).ready(function() {
-    const apiKey = '389d7b50532c55e84bfb73fbd4b66fe8';
 
-    $('.search').submit(function(event) {
+// This function will be called when the user clicks the search button
+$(document).ready(function() {
+    const apiKey = '389d7b50532c55e84bfb73fbd4b66fe8'; // API key for OpenWeatherMap
+
+    $('.search').submit(function(event) { // Click event for search button
         event.preventDefault();
-        const city = $('input[type="search"]').val();
-        fetchCurrentWeather(city);
-        fetchForecast(city);
+        const city = $('input[type="search"]').val(); // Get the value of the search input
+        fetchCurrentWeather(city); // Fetch current weather data
+        fetchForecast(city); // Fetch forecast data
     });
 
     function fetchCurrentWeather(city) {
         const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-        $.get(endpoint, function(data) {
-            $('.city').text(data.name);
-            $('.temp').text(`Temperature: ${data.main.temp}°F`);
-            $('.humid').text(`Humidity: ${data.main.humidity}%`);
-            $('.wind').text(`Wind Speed: ${data.wind.speed} MPH`);
+        $.get(endpoint, function(data) { // Make a GET request to the endpoint
+            $('.city').text(data.name); // Update city name
+            $('.temp').text(`Temperature: ${data.main.temp}°F`); // Update temperature
+            $('.humid').text(`Humidity: ${data.main.humidity}%`); // Update humidity
+            $('.wind').text(`Wind Speed: ${data.wind.speed} MPH`); // Update wind speed
         });
     }
 
     function fetchForecast(city) {
         const endpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
-        $.get(endpoint, function(data) {
-            for (let i = 0; i < 5; i++) {
-                const forecastData = data.list[i];
-                $(`.forecast-info:eq(${i}) .temp`).text(`Temperature: ${forecastData.main.temp}°F`);
-                $(`.forecast-info:eq(${i}) .humid`).text(`Humidity: ${forecastData.main.humidity}%`);
-                $(`.forecast-info:eq(${i}) .wind`).text(`Wind Speed: ${forecastData.wind.speed} MPH`);
-                const forecastIcon = weatherIcon(forecastData.weather[0].main);
-                $(`.forecast-card:eq(${i}) .icon`).attr('src', `./assets/img/${forecastIcon}`);
+        $.get(endpoint, function(data) { // Make a GET request to the endpoint
+            for (let i = 0; i < 5; i++) { // Loop through the next 5 days
+                const forecastData = data.list[i]; // Get the forecast data for the current day
+                $(`.forecast-info:eq(${i}) .temp`).text(`Temperature: ${forecastData.main.temp}°F`); // Update temperature
+                $(`.forecast-info:eq(${i}) .humid`).text(`Humidity: ${forecastData.main.humidity}%`); // Update humidity
+                $(`.forecast-info:eq(${i}) .wind`).text(`Wind Speed: ${forecastData.wind.speed} MPH`); // Update wind speed
+                const forecastIcon = weatherIcon(forecastData.weather[0].main); // Get the icon name
+                $(`.forecast-card:eq(${i}) .icon`).attr('src', `./assets/img/${forecastIcon}`); // Update icon
             }
         });
     }
 });
 
-function weatherIcon(weatherDescription) {
+function weatherIcon(weatherDescription) { // Function to get the icon name based on the weather description
     switch (weatherDescription) {
         case 'Clear':
             return 'clear.png';
@@ -58,7 +61,7 @@ function weatherIcon(weatherDescription) {
 const searchForm = document.querySelector('.search');
 const recentSearches = document.querySelector('.recent-searches');
 
-searchForm.addEventListener('submit', function(event) {
+searchForm.addEventListener('submit', function(event) { // Click event for search button for local storage
     event.preventDefault();
     const cityNameInput = event.target.querySelector('input[type="search"]');
     const cityNameValue = cityNameInput.value.trim();
@@ -72,14 +75,14 @@ searchForm.addEventListener('submit', function(event) {
     }
 });
 
-recentSearches.addEventListener('click', function(event) {
+recentSearches.addEventListener('click', function(event) { // Click event for recent searches
     if (event.target.tagName === 'P') {
         const cityNameFromRecent = event.target.textContent;
         fetchWeatherData(cityNameFromRecent);
     }
 });
 
-function displayWeather(data) {
+function displayWeather(data) { // Function to display weather data
     document.querySelector('.city').textContent = data.name;
     document.querySelector('.temp').textContent = `Temperature: ${data.temp}°F`;
     document.querySelector('.humid').textContent = `Humidity: ${data.humidity}%`;
@@ -88,16 +91,16 @@ function displayWeather(data) {
     document.querySelector('.current-weather .icon').src = `./assets/img/${weatherIconName}`;
 }
 
-function fetchWeatherData(city) {
+function fetchWeatherData(city) { // Function to fetch weather data
     const apiKey = '389d7b50532c55e84bfb73fbd4b66fe8';
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-    fetch(apiURL)
+    fetch(apiURL) 
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            const weatherData = {
+            const weatherData = { // Object to store weather data
                 city: data.name,
                 temp: data.main.temp,
                 humidity: data.main.humidity,
@@ -107,7 +110,7 @@ function fetchWeatherData(city) {
         displayWeather(weatherData);
     });
 }
-$(document).ready(function() {
+$(document).ready(function() { 
     // Show recent searches when the input is focused
     $(".search input").on("focus", function() {
         $(".recent-searches").fadeIn(200);
@@ -121,7 +124,7 @@ $(document).ready(function() {
     });
 });
 
-function addRecentSearch(city) {
+function addRecentSearch(city) { // Function to add recent searches to local storage
     const searches = JSON.parse(localStorage.getItem('recentSearches')) || [];
 
     const index = searches.indexOf(city);
@@ -137,7 +140,7 @@ function addRecentSearch(city) {
     renderRecentSearches();
 }
 
-function renderRecentSearches() {
+function renderRecentSearches() { // Function to render recent searches
     const searches = JSON.parse(localStorage.getItem('recentSearches')) || [];
     recentSearches.innerHTML = searches.map(city => `<p>${city}</p>`).join('');
     recentSearches.style.display = searches.length ? 'block' : 'none';
@@ -145,6 +148,7 @@ function renderRecentSearches() {
 
 renderRecentSearches();
 
+// Show recent searches when the input is clicked
 searchForm.querySelector('input[type="search"]').addEventListener('click', function() {
     const searches = JSON.parse(localStorage.getItem('recentSearches')) || [];
     if (searches.length) {
